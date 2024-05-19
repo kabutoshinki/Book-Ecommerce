fd03922eaa99db33460bc8b15a35cb7c5574f039
Huy <kabutoshinki@gmail.com>
2024-05-19 22:11:31 +0700

    design admin page + create pages for manage + create folder css/js/images + modify app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,48 +12,84 @@ import { ApiTags } from '@nestjs/swagger';
 import { RedisConfigService } from 'config/redis.config';
 import { RedisService } from 'nestjs-redis';
 import { CacheKey } from '@nestjs/cache-manager';
+import { BooksService } from './books/books.service';
+import { CategoriesService } from './categories/categories.service';
 
 @Controller()
 @ApiTags('Default')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly bookService: BooksService,
+    private readonly categoryService: CategoriesService,
+  ) {}
 
   @Get()
   @Render('pages/index')
   root() {
-    return { title: 'Index Page', layout: 'layouts/layout', name: 'Guest' };
+    return {
+      title: 'Dashboard Page',
+      layout: 'layouts/layout',
+      name: 'Guest',
+    };
   }
 
-  @Get('about')
+  @Get('page/about')
   @Render('pages/about')
   about() {
     return { title: 'About Page', name: 'huy' };
   }
-  @Get('login')
+
+  @Get('page/login')
   @Render('pages/login')
   login() {
-    return { title: 'Login Page', name: 'huy' };
+    return { title: 'Login Page', layout: false, name: 'huy' };
   }
-  @Post()
-  async setCacheKey(@Query('key') key: string, @Query('value') value: string) {
-    await this.appService.setCacheKey(key, value);
-    return {
-      success: 200,
-      status: 201,
-      message: 'Key cached successfully',
-    };
-  }
-  @Get('get/:key')
-  async getCacheKey(@Param('key') key: string) {
-    const data = await this.appService.getCacheKey(key);
-    console.log(data);
 
-    return {
-      success: 200,
-      status: 201,
-      data,
-    };
+  @Get('page/user')
+  @Render('pages/user')
+  user() {
+    return { title: 'User Page', name: 'huy' };
   }
+
+  @Get('page/book')
+  @Render('pages/book')
+  async book() {
+    const books = await this.bookService.findAll();
+    return { title: 'Book Page', books: books };
+  }
+
+  @Get('page/author')
+  @Render('pages/author')
+  author() {
+    return { title: 'Author Page', name: 'huy' };
+  }
+
+  @Get('page/category')
+  @Render('pages/category')
+  async category() {
+    const categories = await this.categoryService.findAll();
+    return { title: 'Category Page', categories: categories };
+  }
+
+  @Get('page/order')
+  @Render('pages/order')
+  order() {
+    return { title: 'Order Page', name: 'huy' };
+  }
+
+  @Get('page/discount')
+  @Render('pages/discount')
+  discount() {
+    return { title: 'Discount Page', layout: 'layouts/layout', name: 'huy' };
+  }
+
+  @Get('page/publisher')
+  @Render('pages/publisher')
+  publisher() {
+    return { title: 'Publisher Page', name: 'huy' };
+  }
+
   @Get('test/errors')
   testErrors(): string {
     throw new UnauthorizedException({ message: 'unknown error' });
