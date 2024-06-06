db4dd02df1f4206aba5be8b860178fe4be622bb5
Huy <kabutoshinki@gmail.com>
2024-06-07 00:08:32 +0700

    Test: Create Cart Test

--- src/cart/cart.controller.spec.ts
+++ src/cart/cart.controller.spec.ts
@@ -3,9 +3,12 @@ import { CartController } from './cart.controller';
 import { CartService } from './cart.service';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { BooksService } from '../books/books.service';
+import { AddToCartDto } from './dto/requests/add-to-cart.dto';
+import { UpdateCartDto } from './dto/requests/update-cart.dto';
 
 describe('CartController', () => {
   let controller: CartController;
+  let cartService: CartService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -30,9 +33,120 @@ describe('CartController', () => {
     }).compile();
 
     controller = module.get<CartController>(CartController);
+    cartService = module.get<CartService>(CartService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('addToCart', () => {
+    it('should add item to cart', async () => {
+      const addToCartDto: AddToCartDto = {
+        userId: 'testUserId',
+        bookId: 'testBookId',
+        quantity: 1,
+      };
+
+      jest.spyOn(cartService, 'addToCart').mockResolvedValueOnce(undefined);
+
+      const result = await controller.addToCart(addToCartDto);
+
+      expect(result).toEqual('Item added to cart');
+      expect(cartService.addToCart).toHaveBeenCalledWith(addToCartDto);
+    });
+  });
+
+  describe('getCartQuantity', () => {
+    it('should return cart quantity', async () => {
+      const userId = 'testUserId';
+      const cartQuantity = 5;
+
+      jest
+        .spyOn(cartService, 'getCartQuantity')
+        .mockResolvedValueOnce(cartQuantity);
+
+      const result = await controller.getCartQuantity(userId);
+
+      expect(result).toEqual(cartQuantity);
+      expect(cartService.getCartQuantity).toHaveBeenCalledWith(userId);
+    });
+  });
+
+  describe('getCart', () => {
+    it('should return user cart', async () => {
+      const userId = 'testUserId';
+      const userCart = [
+        { bookId: '1', quantity: 2 },
+        { bookId: '2', quantity: 1 },
+      ];
+
+      jest.spyOn(cartService, 'getCart').mockResolvedValueOnce(userCart);
+
+      const result = await controller.getCart(userId);
+
+      expect(result).toEqual(userCart);
+      expect(cartService.getCart).toHaveBeenCalledWith(userId);
+    });
+  });
+
+  describe('increaseQuantity', () => {
+    it('should increase quantity of user cart item', async () => {
+      const updateCartDto: UpdateCartDto = {
+        userId: 'testUserId',
+        bookId: 'testBookId',
+        quantity: 1,
+      };
+
+      jest
+        .spyOn(cartService, 'increaseUserCartItemQuantity')
+        .mockResolvedValueOnce();
+
+      const result = await controller.increaseQuantity(updateCartDto);
+
+      expect(result).toEqual('Item quantity increased');
+      expect(cartService.increaseUserCartItemQuantity).toHaveBeenCalledWith(
+        updateCartDto.userId,
+        updateCartDto,
+      );
+    });
+  });
+
+  describe('decreaseQuantity', () => {
+    it('should decrease quantity of user cart item', async () => {
+      const updateCartDto: UpdateCartDto = {
+        userId: 'testUserId',
+        bookId: 'testBookId',
+        quantity: 1,
+      };
+
+      jest
+        .spyOn(cartService, 'decreaseUserCartItemQuantity')
+        .mockResolvedValueOnce();
+
+      const result = await controller.decreaseQuantity(updateCartDto);
+
+      expect(result).toEqual('Item quantity decreased');
+      expect(cartService.decreaseUserCartItemQuantity).toHaveBeenCalledWith(
+        updateCartDto.userId,
+        updateCartDto,
+      );
+    });
+  });
+
+  describe('deleteCartItem', () => {
+    it('should delete item from user cart', async () => {
+      const userId = 'testUserId';
+      const bookId = 'testBookId';
+
+      jest.spyOn(cartService, 'deleteCartItem').mockResolvedValueOnce();
+
+      const result = await controller.deleteCartItem(userId, bookId);
+
+      expect(result).toEqual(
+        `Item with bookId ${bookId} deleted from user's cart`,
+      );
+      expect(cartService.deleteCartItem).toHaveBeenCalledWith(userId, bookId);
+    });
   });
 });
--- src/cart/cart.service.spec.ts
+++ src/cart/cart.service.spec.ts
@@ -5,18 +5,21 @@ import { BooksService } from '../books/books.service';
 
 describe('CartService', () => {
   let service: CartService;
+  let cacheManagerMock: any;
 
   beforeEach(async () => {
+    cacheManagerMock = {
+      get: jest.fn(),
+      set: jest.fn(),
+      del: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CartService,
         {
           provide: CACHE_MANAGER,
-          useValue: {
-            get: jest.fn(),
-            set: jest.fn(),
-            del: jest.fn(),
-          }, // Mock cache manager
+          useValue: cacheManagerMock, // Mock cache manager
         },
         {
           provide: BooksService,
@@ -30,5 +33,94 @@ describe('CartService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('addToCart', () => {
+    it('should add item to cart', async () => {
+      const addToCartDto = {
+        userId: 'testUserId',
+        bookId: 'testBookId',
+        quantity: 1,
+      };
+      const cartKey = `cart:${addToCartDto.userId}`;
+      const cartItem = {
+        bookId: addToCartDto.bookId,
+        quantity: addToCartDto.quantity,
+      };
+
+      cacheManagerMock.get.mockResolvedValueOnce(null);
+      cacheManagerMock.set.mockResolvedValueOnce();
+
+      const result = await service.addToCart(addToCartDto);
+
+      expect(result).toEqual([cartItem]);
+      expect(cacheManagerMock.get).toHaveBeenCalledWith(cartKey);
+      expect(cacheManagerMock.set).toHaveBeenCalledWith(cartKey, [cartItem]);
+    });
+  });
+
+  describe('getCartQuantity', () => {
+    it('should return cart quantity', async () => {
+      const userId = 'testUserId';
+      const cartKey = `cart:${userId}`;
+      const userCart = [
+        { bookId: '1', quantity: 2 },
+        { bookId: '2', quantity: 1 },
+      ];
+
+      cacheManagerMock.get.mockResolvedValueOnce(userCart);
+
+      const result = await service.getCartQuantity(userId);
+
+      expect(result).toEqual(userCart.length);
+      expect(cacheManagerMock.get).toHaveBeenCalledWith(cartKey);
+    });
+
+    it('should return 0 if cart is empty', async () => {
+      const userId = 'testUserId';
+      const cartKey = `cart:${userId}`;
+
+      cacheManagerMock.get.mockResolvedValueOnce([]);
+
+      const result = await service.getCartQuantity(userId);
+
+      expect(result).toEqual(0);
+      expect(cacheManagerMock.get).toHaveBeenCalledWith(cartKey);
+    });
+  });
+
+  describe('clearCart', () => {
+    it('should clear user cart', async () => {
+      const userId = 'testUserId';
+      const cartKey = `cart:${userId}`;
+
+      cacheManagerMock.del.mockResolvedValueOnce();
+
+      await service.clearCart(userId);
+
+      expect(cacheManagerMock.del).toHaveBeenCalledWith(cartKey);
+    });
+  });
+
+  describe('increaseUserCartItemQuantity', () => {
+    it('should increase quantity of user cart item', async () => {
+      const userId = 'testUserId';
+      const updateCartDto = {
+        userId: 'testUserId',
+        bookId: 'testBookId',
+        quantity: 1,
+      };
+      const userCart = [{ bookId: 'testBookId', quantity: 1 }];
+
+      cacheManagerMock.get.mockResolvedValueOnce(userCart);
+      cacheManagerMock.set.mockResolvedValueOnce();
+
+      await service.increaseUserCartItemQuantity(userId, updateCartDto);
+
+      expect(cacheManagerMock.get).toHaveBeenCalledWith(`cart:${userId}`);
+      expect(cacheManagerMock.set).toHaveBeenCalledWith(`cart:${userId}`, [
+        { bookId: 'testBookId', quantity: 2 },
+      ]);
+    });
   });
 });
